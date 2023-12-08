487be1f548fb632eacf989063b3034c92c8e30b7
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-12-08 11:32:14 +0530

    node-demos

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 
 console.log('start');
 
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    console.log('welcome');
+    res.send('Welcome to Deloitte NodeJS demo application.');
+})
+
+app.listen(() => {
+    console.log(`App is running at http://localhost:${port}/`);
+}, port);
+
